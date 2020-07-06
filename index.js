e2ed3b664c37ef504d68b3f5f9c8600cf1fa9780
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-06 13:28:41 -0400

    Hubs and lessons routes

--- index.js
+++ index.js
@@ -4,8 +4,42 @@ const server = express();
 
 const PORT = 8000;
 
+let hubs = [
+  {
+    id: 0,
+    name: "Hub 1",
+    lessonId: 0,
+    cohort: "web 30",
+  },
+  {
+    id: 1,
+    name: "Hub 2",
+    lessonId: 1,
+    cohort: "web 30",
+  },
+];
+
+let lessons = [
+  {
+    id: 0,
+    name: "Lesson 1",
+  },
+  {
+    id: 1,
+    name: "Lesson 2",
+  },
+];
+
 server.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
+});
+
+server.get("/api/hubs", (req, res) => {
+  res.json(hubs);
+});
+
+server.get("/api/lessons", (req, res) => {
+  res.json(lessons);
 });
 
 server.listen(PORT, () => {
