a9367a7af1aca116990c1b7fd510311f184b6350
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-06 13:55:07 -0400

    New routes with express.json

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express");
+const shortId = require("shortid");
 
 const server = express();
+
+server.use(express.json());
 
 const PORT = 8000;
 
 let hubs = [
   {
-    id: 0,
+    id: shortId.generate(),
     name: "Hub 1",
     lessonId: 0,
     cohort: "web 30",
   },
   {
-    id: 1,
+    id: shortId.generate(),
     name: "Hub 2",
     lessonId: 1,
     cohort: "web 30",
@@ -40,6 +43,27 @@ server.get("/api/hubs", (req, res) => {
 
 server.get("/api/lessons", (req, res) => {
   res.json(lessons);
+});
+
+server.post("/api/hubs", (req, res) => {
+  if (req.body === undefined) {
+    return res.status(400).send("Body Required.");
+  }
+  const newHub = req.body;
+  newHub.id = shortId.generate();
+  hubs.push(newHub);
+  res.json(newHub);
+});
+
+server.post("/api/lessons", (req, res) => {
+  if (req.body === undefined) {
+    return res.status(400).send("Body Required.");
+  }
+
+  const newLesson = req.body;
+  newLesson.id = shortId.generate();
+  lessons.push(newLesson);
+  res.json(newLesson);
 });
 
 server.listen(PORT, () => {
